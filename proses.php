fc89b4fd5a66f1e2ef917b05e88571aa330a3488
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-09-24 10:55:59 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,24 @@
 <?php
+$foto = $_FILES['gambar']['name'];
+if (isset($_POST['submit'])) {
+	move_uploaded_file($_FILES['gambar']['tmp_name'], "upload/".$_FILES['gambar']['name']);
+	echo "Foto Anda : ";
+	echo "<br>";
+	echo "<img src = 'upload/$foto' >";
+}
 
+$hobi = $_POST["hobi"];
+echo "<br>";
+echo "Hobi yang Anda Pilih :";
+echo "<br>";
+foreach ($hobi as $kesukaan) {
+	echo "- $kesukaan";
+	echo "<br>";
+}
+if (isset($_POST['hapus'])) {
+	unset($hobi);
+}
 ?>
+<form action="halamanakhir.php">
+<input type="submit" name="hapus" value="Hapus">
+</form>
